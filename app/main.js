95e3999a1abb534a6c0fe37a5b339cb2a277c466
Daniel Fasula <danielfasula@gmail.com>
2021-02-17 18:42:55 -0700

    navbar and tabs all fixed

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ import JobsController from "./Controllers/JobsController.js"
 // import ValuesController from "./Controllers/ValuesController.js";
 
 function tab() {
-  let triggerTabList = [].slice.call(document.querySelectorAll('#nav-houses-tab, #nav-cars-tab'))
+  let triggerTabList = [].slice.call(document.querySelectorAll('#nav-houses-tab, #nav-cars-tab, #nav-home-tab, #nav-jobs-tab'))
   triggerTabList.forEach(function (triggerEl) {
     var tabTrigger = new bootstrap.Tab(triggerEl)
     triggerEl.addEventListener('click', function (event) {
